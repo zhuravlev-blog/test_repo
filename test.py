a6997e4a9e449162611aee6c49d59c65df1bb510
zhuravlev-blog-kraken <v.zyravlev@yandex.ru>
2022-03-05 00:37:26 +0300

    three commit

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 
-var1 = 200
-var2 = 500
-print(var1+var2) #700
+x1 = abs(-7.25)
+print(x1) #7.25
 
+x2 = abs(3+5j)
+print(x2) #5.830951894845301
+
+x3 = 3+5j
+print(x3) #(3+5j)
+
+x4 = abs(5j)
+print(x4) #5.0
